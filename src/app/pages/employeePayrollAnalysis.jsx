e82f26d99a3d13ef1ee7c253a02d13b6a6148d70
sourabhpande532 <sourabhpande43@gmail.com>
2025-09-23 21:18:11 +0530

    Calc: total hours,salaryFourWeek,bonus

--- src/app/pages/employeePayrollAnalysis.jsx
+++ src/app/pages/employeePayrollAnalysis.jsx
@@ -1,4 +1,31 @@
+import { employeeData } from "../../data/employeeData"
 import { EmployeeHeader } from "../component/employeeHeader"
+
+const calculateEmployeesAnalysis = employeeData.map((eachEmployee)=>{
+  const {hourlySalary,week1Hours,week2Hours,week3Hours,week4Hours,bonusMultiplier} = eachEmployee;
+  const totalHours = week1Hours + week2Hours + week3Hours + week4Hours;
+  const getTotalSalaryFourWeeks = totalHours * hourlySalary;
+  const bonusThreshold = 160; // 40 * 4 i.e 160
+  let extraBonusHours = 0;
+  if(totalHours > bonusThreshold){
+    extraBonusHours = totalHours - bonusThreshold;
+  }else{
+    extraBonusHours = 0;
+  }
+
+  const bonusAmount = extraBonusHours * hourlySalary * (bonusMultiplier / 100);
+
+  const totalCompensation = getTotalSalaryFourWeeks + bonusAmount;
+
+  return {
+    ...eachEmployee,
+    totalHours,
+    getTotalSalaryFourWeeks,
+    bonusAmount,
+    totalCompensation
+  }
+})
+// console.log(calculateEmployeesAnalysis);
 
 export const PayrollAnalysis = () => {
     return(
